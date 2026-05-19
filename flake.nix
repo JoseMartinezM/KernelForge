8c127c8b87379e8fb5e3a93f1fda1d0f430b5902
Victor Quintana <435victorjavier@gmail.com>
2026-05-19 12:07:19 -0600

    chore(notebooks/benchmark.py): tweak regex, use marimo builtin cache

--- flake.nix
+++ flake.nix
@@ -17,26 +17,51 @@
     {
       devShells = forAllSystems (system:
         let
-          pkgs = import nixpkgs { inherit system; };
-
-          nativeLibs = builtins.attrValues {
+          baseNativeLibs = pkgs: builtins.attrValues {
             inherit (pkgs)
               zlib
+              zstd
               blas
               lapack
               ;
 
             libstd = pkgs.stdenv.cc.cc.lib;
           };
+
+          basePackages = pkgs: [
+            pkgs.uv
+            pkgs.nodejs-slim_22
+          ];
         in
         {
-          default = pkgs.mkShell {
-            packages = [ pkgs.python3 pkgs.uv pkgs.nodejs-slim_22 ];
+          default = let
+            pkgs = import nixpkgs { inherit system; };
+          in pkgs.mkShell {
+            packages = basePackages pkgs;
 
             shellHook = ''
               export UV_PROJECT_ENVIRONMENT="$PWD/.venv"
               source .venv/bin/activate
-              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath nativeLibs}:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath (baseNativeLibs pkgs)}:$LD_LIBRARY_PATH"
+            '';
+          };
+
+          rocm = let
+            pkgs = import nixpkgs {
+              inherit system;
+              config.rocmSupport = true;
+            };
+          in pkgs.mkShell {
+            packages = basePackages pkgs ++ [
+              pkgs.rocmPackages.rocminfo
+              pkgs.rocmPackages.clr
+              pkgs.rocmPackages.llvm.clang
+            ];
+
+            shellHook = ''
+              export UV_PROJECT_ENVIRONMENT="$PWD/.venv"
+              source .venv/bin/activate
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath (baseNativeLibs pkgs)}:$LD_LIBRARY_PATH"
             '';
           };
         });
